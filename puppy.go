9c439a84cc5ae3f2b1fbf92772ad439da179def7
Aditya <darkwhiteknightjs@gmail.com>
2025-05-13 21:53:30 +0530

    added tags

--- puppy.go
+++ puppy.go
@@ -1,5 +1,9 @@
 package puppy
 
+import (
+	"github.com/aditya020402/dog1"
+)
+
 func Bark() string {
 	return "Woof!!"
 }
@@ -8,10 +12,14 @@ func Barks() string {
 	return "Woof !! Woof !! Woof !!"
 }
 
-// func BigBark() string {
-// 	return dog.WhenGrownUp(Bark())
-// }
+func BigBark() string {
+	return dog1.WhenGrownUp(Bark())
+}
 
-// func BigBarks() string {
-// 	return dog.WhenGrownUp(Barks())
-// }
+func BigBarks() string {
+	return dog1.WhenGrownUp(Barks())
+}
+
+func Eating() string {
+	return "Puppy is eating food."
+}
